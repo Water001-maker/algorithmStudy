b85b225862651bc547823914b8737b3ce2733754
幻想家懒得想了 <2579136303@qq.com>
2023-01-12 15:22:21 +0800

    归并排序非递归实现方式

--- src/com/algorithm/newStudy/sort/high/MergeSort.java
+++ src/com/algorithm/newStudy/sort/high/MergeSort.java
@@ -38,7 +38,7 @@ public class MergeSort {
         int j = rightPtr;
         int k = 0;
 
-        //
+        //左右比较，将小的加到temp数组中
         while (i <= mid && j <= rightBound) {
             temp[k++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];
         }
@@ -50,6 +50,53 @@ public class MergeSort {
 
         //将temp的值复制到arr中
         for (int m = 0; m < temp.length; m++) arr[leftPtr + m] = temp[m];
+
+    }
+
+    /**
+     * 非递归
+     * 越界问题
+     * @param arr
+     */
+    public static void merge2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        int step = 1;//步长
+        while (step < N) {
+            int L = 0;//左指针
+            while (L < N) {
+                int M = 0;//每一组的尾指针
+                //当最后一个左组是否长度足够？
+                if (N - L >= step) {//当最后一个左组长度足够时
+                    M = L + step - 1;
+                } else {//当最后一个左组长度不够时
+                    M = N - 1;
+                }
+                if (M == N - 1) {//当尾指针等于数组长度时
+                    break;
+                }
+                int R = 0;//右指针
+                //左指针+步长是否大于数组长度？
+                if (N - 1 - M >= step) {//足够
+                    R = M + step;
+                } else {//不足
+                    R = N - 1;
+                }
+                merge(arr, L, M, R);//归并
+                //右指针长度是否等于数组长度
+                if (R == N - 1) {
+                    break;
+                } else {//不等于时，新的左指针下标=右指针+1
+                    L = R + 1;
+                }
+            }
+            if (step > N / 2) {//长步长大于数组长度的一半时，就不用继续了。ps.N / 2向下取整
+                break;
+            }
+            step *= 2;//步长*2
+        }
 
     }
 

123c8f23ed33033b6bccfc1388635f5069de9db7
幻想家懒得想了 <2579136303@qq.com>
2023-01-14 18:32:56 +0800

    用对数器验证算法的可靠性

--- src/com/algorithm/newStudy/array/OddTimesNum.java
+++ src/com/algorithm/newStudy/array/OddTimesNum.java
@@ -1,5 +1,8 @@
 package com.algorithm.newStudy.array;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * @author xuemiao
  * @date 2023/1/14 15:43
@@ -62,12 +65,12 @@ public class OddTimesNum {
         int[] bytes = new int[32];
 
         for (int num : arr) {
-            for (int i = 0; i < 31; i++) {
+            for (int i = 0; i < 32; i++) {
                 bytes[i] += ((num >> i) & 1);
             }
         }
         int res = 0;
-        for (int i = 0; i < 31; i++) {
+        for (int i = 0; i < 32; i++) {
             if (bytes[i] % m == 0) {
                 continue;
             }
@@ -80,10 +83,126 @@ public class OddTimesNum {
         if (res == 0) {//边界处理
             int count = 0;
             for (int num : arr) {
-                if (num == 0) count++;
+                if (num == 0) {
+                    count++;
+                }
             }
-            if (count != k) return -1;
+            if (count != k) {
+                return -1;
+            }
         }
         return res;
     }
+
+    //对数器——用另一种方式实现，并确认
+
+    //另一种实现：请保证arr中，只有一种数出现了K次，其他数都出现了M次
+    public static int test(int[] arr, int k, int m) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        for (int num : map.keySet()) {
+            if (map.get(num) == k) {
+                return num;
+            }
+        }
+        return -1;
+    }
+
+    public static void mapCreater(HashMap<Integer, Integer> map) {
+        int value = 1;
+        for (int i = 0; i < 32; i++) {
+            map.put(value, i);
+            value <<= 1;
+        }
+    }
+
+    /**
+     * 对数器
+     *
+     * @param maxKinds 几个数字
+     * @param range    值的范围
+     * @param k        k次
+     * @param m        m次
+     * @return
+     */
+    public static int[] randomArray(int maxKinds, int range, int k, int m) {
+        int ktimeNum = randomNumber(range);
+        // 真命天子出现的次数
+        int times = Math.random() < 0.5 ? k : ((int) (Math.random() * (m - 1)) + 1);
+        int numKinds = (int) (Math.random() * maxKinds) + 2;
+        // k * 1 + (numKinds - 1) * m
+        int[] arr = new int[times + (numKinds - 1) * m];
+        int index = 0;
+        for (; index < times; index++) {
+            arr[index] = ktimeNum;//存k个
+        }
+        numKinds--;
+        HashSet<Integer> set = new HashSet<>();
+        set.add(ktimeNum);
+        while (numKinds != 0) {
+            int curNum = 0;
+            do {
+                curNum = randomNumber(range);
+            } while (set.contains(curNum));
+            set.add(curNum);
+            numKinds--;
+            for (int i = 0; i < m; i++) {
+                arr[index++] = curNum;//存m个
+            }
+        }
+        // arr 填好了，但是太有序，所以打乱
+        for (int i = 0; i < arr.length; i++) {
+            // i 位置的数，我想随机和j位置的数做交换
+            int j = (int) (Math.random() * arr.length);// 0 ~ N-1
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+        return arr;
+    }
+
+
+    /**
+     * @param range [-range, +range]取值范围
+     * @return
+     */
+    public static int randomNumber(int range) {
+        return (int) (Math.random() * (range + 1)) - (int) (Math.random() * (range + 1));
+    }
+
+    public static void main(String[] args) {
+        int kinds = 5;//几个数字
+        int range = 30;//值的范围-30-30
+        int testTime = 100000;//次数
+        int max = 9;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int a = (int) (Math.random() * max) + 1; // a 1 ~ 9
+            int b = (int) (Math.random() * max) + 1; // b 1 ~ 9
+            // k < m
+            int k = Math.min(a, b);
+            int m = Math.max(a, b);
+            if (k == m) {
+                m++;
+            }
+            int[] arr = randomArray(kinds, range, k, m);
+            int ans1 = printKTimesNum(arr, k, m);
+            int ans2 = test(arr, k, m);
+            if (ans1 != ans2) {
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println("出错了！");
+            }
+        }
+        System.out.println("测试结束");
+
+    }
+
+
 }
